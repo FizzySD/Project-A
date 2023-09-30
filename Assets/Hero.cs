f4c8c257c27a254af5d1140edea8e239d85e7964
birkirarndal <birkir30@gmail.com>
2023-09-30 13:43:40 +0000

    hero_state and dodge

--- Assets/Hero.cs
+++ Assets/Hero.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Threading;
 using UnityEngine;
 
+
 public class Hero : MonoBehaviour
 {
     #region Comments
@@ -11,6 +12,7 @@ public class Hero : MonoBehaviour
     #endregion
     public static Hero instance;
     private Rigidbody rb;
+    private HERO_STATE _state;
     public float currentSpeed;
     private Animator anim;
     public float speed = 9.5f;
@@ -19,6 +21,9 @@ public class Hero : MonoBehaviour
     private readonly float gravity = 20f;
     private bool grounded;
     public bool canMove = true;
+    private float dashTime;
+    private float facingDirection;
+    private Quaternion targetRotation;
 
     // Start is called before the first frame update
     private void Awake()
@@ -69,6 +74,7 @@ public class Hero : MonoBehaviour
     public void InputHandler() 
     {
         float num2 = 0;
+        float num = 0;
         if(Input.GetKey(KeyCode.W))
         {
             num2 = 1f;
@@ -77,7 +83,6 @@ public class Hero : MonoBehaviour
         {
             num2 = -1f;
         }
-        float num = 0;
         if (Input.GetKey(KeyCode.A)) 
         {
             num = -1f;
@@ -88,37 +93,75 @@ public class Hero : MonoBehaviour
         }
         if (grounded && canMove) 
         {
-
             Vector3 vector5 = Vector3.zero;
-            Vector3 vector6 = new Vector3(num, 0f, num2);
-            float num3 = getGlobalFacingDirection(num, num2);
-            vector5 = getGlobaleFacingVector3(num3);
-            float num4 = ((!(vector6.magnitude <= 0.95f)) ? 1f : ((vector6.magnitude >= 0.25f) ? vector6.magnitude : 0f));
-            vector5 *= num4;
-            vector5 *= speed;
+            switch(this.state) {
+                case HERO_STATE.GroundDodge:
+                    {
+                        if (anim.GetCurrentAnimatorStateInfo(0).IsName("dodge") && anim.GetCurrentAnimatorStateInfo(0).normalizedTime >= 0.2f && anim.GetCurrentAnimatorStateInfo(0).normalizedTime < 0.8f)
+                        {
+                            vector5 = -transform.forward * 2.4f * speed;
+                        }
+
+                        if (anim.GetCurrentAnimatorStateInfo(0).IsName("dodge") && anim.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.8f)
+                        {
+                            vector5 =  rb.velocity;
+                            vector5 *= 0.9f;
+                        }
+
+                        break;
+                    }
+
+                case HERO_STATE.Idle:
+                    Vector3 vector6 = new Vector3(num, 0f, num2);
+                    float num3 = getGlobalFacingDirection(num, num2);
+                    vector5 = getGlobaleFacingVector3(num3);
+                    float num4 = (!(vector6.magnitude <= 0.95f)) ? 1f : ((vector6.magnitude >= 0.25f) ? vector6.magnitude : 0f);
+                    vector5 *= num4;
+                    vector5 *= speed;
+                    
+                    if (num != 0f || num2 != 0f) {
+                        anim.SetBool("isRunning", true);
+                    }
+                    else 
+                    {
+                        anim.SetBool("isRunning", false);
+                        num3 = -874f;
+                    }
+
+                    if (num3 != -874f)
+                    {
+                        facingDirection = num3;
+                        targetRotation = Quaternion.Euler(0f, facingDirection, 0f);
+                    }
+
+                    break;
+                
+            }
+
+            
             Vector3 velocity = rb.velocity;
             Vector3 force = vector5 - velocity;
             force.x = Mathf.Clamp(force.x, 0f - maxVelocityChange, maxVelocityChange);
             force.z = Mathf.Clamp(force.z, 0f - maxVelocityChange, maxVelocityChange);
             force.y = 0f;
-
             //smth like that we need to find how to see the current anim
             if (anim.GetCurrentAnimatorStateInfo(0).IsName("jump")) // there
             {
                 force.y += 8f;
             }
-
-            rb.AddForce(force,ForceMode.VelocityChange);
-            rb.rotation = Quaternion.Lerp(base.gameObject.transform.rotation, Quaternion.Euler(0f, num3, 0f), Time.deltaTime * 10f);
-
-            if(vector6.magnitude > 0.1f)
-            {
-                anim.SetBool("isRunning", true);
+            if (state != HERO_STATE.Attack) {
+                rb.AddForce(force, ForceMode.VelocityChange);
+                rb.rotation = Quaternion.Lerp(base.gameObject.transform.rotation, Quaternion.Euler(0f, facingDirection, 0f), Time.deltaTime * 10f);
             }
-            else 
-            {
-                anim.SetBool("isRunning", false); //ok no idea now lol
-            }
+
+            // if(vector6.magnitude > 0.1f)
+            // {
+            //     anim.SetBool("isRunning", true);
+            // }
+            // else 
+            // {
+            //     anim.SetBool("isRunning", false); //ok no idea now lol
+            // }
 
             currentSpeed = rb.velocity.magnitude;
             Debug.Log(vector5);
@@ -151,6 +194,65 @@ public class Hero : MonoBehaviour
         float num = Mathf.Atan2(vertical, horizontal) * 57.29578f;
         num = 0f - num + 90f;
         return y + num;
+    }
+    
+
+    private void dodge(bool offTheWall = false)
+    {
+        
+        this.state = HERO_STATE.GroundDodge;
+        if (!offTheWall)
+        {
+            float num2 = 0;
+            float num = 0;
+            if(Input.GetKey(KeyCode.W))
+            {
+                num2 = 1f;
+            }
+            else if (Input.GetKey(KeyCode.S)) 
+            {
+                num2 = -1f;
+            }
+            if (Input.GetKey(KeyCode.A)) 
+            {
+                num = -1f;
+            }
+            else if(Input.GetKey(KeyCode.D)) 
+            {
+                num = 1f;
+            }
+            float num3 = this.getGlobalFacingDirection(num2, num);
+            if ((num2 != 0f) || (num != 0f))
+            {
+                this.facingDirection = num3 + 180f;
+                this.targetRotation = Quaternion.Euler(0f, this.facingDirection, 0f);
+            }
+            this.anim.SetBool("dodge", true);
+            // this.crossFade("dodge", 0.1f);
+        }
+        else
+        {
+            this.anim.SetBool("dodge", true);
+            // this.playAnimation("dodge");
+            // this.playAnimationAt("dodge", 0.2f);
+        }
+            
+        
+    }
+    private HERO_STATE state
+    {
+        get
+        {
+            return this._state;
+        }
+        set
+        {
+            if ((this._state == HERO_STATE.AirDodge) || (this._state == HERO_STATE.GroundDodge))
+            {
+                this.dashTime = 0f;
+            }
+            this._state = value;
+        }
     }
 }
 
